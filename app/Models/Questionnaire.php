11f2af5e405cd3f7815b454f86d41daee55818a6
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-12-07 12:06:52 +0500

    Lecture 6
    Show questionnaires and place a sharable link with refacturing the url.

--- app/Models/Questionnaire.php
+++ app/Models/Questionnaire.php
@@ -2,13 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Cache\RetrievesMultipleKeys;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Questionnaire extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function path() {
+        return url('/questionnaires/'.$this->id);
+    }
+
+    public function publicPath() {
+        return url('/surveys/'.$this->id.'-'.Str::slug($this->title));
+    }
 
     public function user() {
         return $this->belongsTo(User::class);
